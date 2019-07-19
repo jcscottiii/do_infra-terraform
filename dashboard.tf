15d111290d72b20881efdef5820bd187d37ada8f
James Scott <jcscott.iii@gmail.com>
2019-07-18 21:24:11 -0400

    dashboard

---
+++ dashboard.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "k8s_dashboard" {
+  name       = "kubernetes-dashboard"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart      = "kubernetes-dashboard"
+  version    = "1.7.1"
+  namespace  = "kube-system"
+}
