a55f5e86aff637b174cc004f3e57b4a59a4b1e60
James Scott <jcscott.iii@gmail.com>
2020-03-15 12:07:01 -0400

    fix kubernetes config

--- helm.tf
+++ helm.tf
@@ -6,6 +6,8 @@ provider "helm" {
     client_certificate     = base64decode(digitalocean_kubernetes_cluster.personal.kube_config.0.client_certificate)
     client_key             = base64decode(digitalocean_kubernetes_cluster.personal.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.personal.kube_config.0.cluster_ca_certificate)
+    token                  = digitalocean_kubernetes_cluster.personal.kube_config.0.token
+    load_config_file       = false
   }
 }
 
