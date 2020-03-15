e3e5108ae2a817778761eef036a33fe7adc41560
James Scott <jcscott.iii@gmail.com>
2020-03-15 10:56:15 -0400

    use remote state

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ terraform {
   required_providers {
     helm = "~> v0.10.4"
   }
+  backend "s3" {
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    region                      = "us-west-1"
+    key                         = "do_infra-v2-terraform.tfstate"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+  }
 }
 
 # Configure the DigitalOcean Provider
@@ -11,7 +20,6 @@ provider "digitalocean" {
 
 variable "do_token" {
 }
-
 
 resource "digitalocean_kubernetes_cluster" "personal" {
   name    = "personal"
