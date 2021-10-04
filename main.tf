54ac7cee6aa7cc9d2269ec3cae8f0708ba41a78d
James Scott <jcscott.iii@gmail.com>
2021-10-04 09:35:29 -0400

    fix

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
