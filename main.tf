943e1076f941eec267f6621937dd5dc065b2ca19
James Scott <jcscott.iii@gmail.com>
2021-10-04 09:22:42 -0400

    upgrade

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
   }
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = "~> 2.0"
     }
   }
