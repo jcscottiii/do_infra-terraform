95dfcd969a5cf1a4b19474f53d7ddb6eaed05b7a
James C Scott III <jcscottiii@users.noreply.github.com>
2021-10-04 06:11:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,6 @@ resource "digitalocean_kubernetes_cluster" "personal" {
 }
 
 output "kube_config" {
-  value     = "${digitalocean_kubernetes_cluster.personal.kube_config.0.raw_config}"
+  value     = digitalocean_kubernetes_cluster.personal.kube_config.0.raw_config
   sensitive = true
 }
