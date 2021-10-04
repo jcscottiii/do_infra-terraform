c7bf636717570cf6cf700e91451bf63ade7cc2c9
James Scott <jcscott.iii@gmail.com>
2021-10-04 09:36:43 -0400

    fix

--- helm.tf
+++ helm.tf
@@ -5,7 +5,6 @@ provider "helm" {
     client_key             = base64decode(digitalocean_kubernetes_cluster.personal.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.personal.kube_config.0.cluster_ca_certificate)
     token                  = digitalocean_kubernetes_cluster.personal.kube_config.0.token
-    load_config_file       = false
   }
 }
 
