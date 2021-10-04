736ca8d82078e9f737d71ac83f4fbcdb681a88f1
James Scott <jcscott.iii@gmail.com>
2021-10-04 09:17:06 -0400

    fix

--- main.tf
+++ main.tf
@@ -3,9 +3,7 @@ terraform {
     endpoint                    = "nyc3.digitaloceanspaces.com"
     region                      = "us-west-1"
     key                         = "do_infra-v2-terraform.tfstate"
-    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
   }
 }
